f0a4729d3d6b4d3fc543a7c5c4fea0e0c01fd0df
Mina Samy <m.s.ramzy@gmail.com>
2015-10-25 00:10:06 +0200

    Initial Commit

---
+++ src/main/java/bloodstone/dailyselfie/cloud/DailySelfieCloudApplication.java
@@ -0,0 +1,12 @@
+package bloodstone.dailyselfie.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailySelfieCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailySelfieCloudApplication.class, args);
+    }
+}
---
+++ src/test/java/bloodstone/dailyselfie/cloud/DailySelfieCloudApplicationTests.java
@@ -0,0 +1,18 @@
+package bloodstone.dailyselfie.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DailySelfieCloudApplication.class)
+@WebAppConfiguration
+public class DailySelfieCloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
